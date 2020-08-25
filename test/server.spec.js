cbc25708a783aca9fcad34df2ca1ce56eb4121ce
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-08-25 18:49:30 -0300

    Add more tests.

--- test/server.spec.js
+++ test/server.spec.js
@@ -29,8 +29,19 @@ describe('jhipster-jooq:server sub-generator (Server blueprint)', function () {
             runResult.assertFile('jooq.xml');
         });
 
+        it('writes application-jooq.yml', () => {
+            runResult.assertFile('src/main/resources/config/application-jooq.yml');
+        });
+
+        it('adds jooq.version property to pom.xml', () => {
+            runResult.assertFileContent('pom.xml', /<jooq.version>.*<\/jooq.version>/);
+        });
+
         it('adds jooq dependency and executions to pom.xml', () => {
-            runResult.assertFileContent('pom.xml', 'spring-boot-starter-jooq');
+            runResult.assertFileContent('pom.xml', '<artifactId>spring-boot-starter-jooq</artifactId>');
+            runResult.assertFileContent('pom.xml', '<artifactId>jooq-codegen-maven</artifactId>');
+            runResult.assertFileContent('pom.xml', '<artifactId>jooq</artifactId>');
+            runResult.assertFileContent('pom.xml', '<artifactId>jooq-meta-extensions</artifactId>');
         });
     });
 });
