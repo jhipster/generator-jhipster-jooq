7a8146a8dfb2cc5b242b69479fc367b291d6ff63
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-09-15 12:37:26 -0300

    Add option to make jooq optional.

--- test/entity-server.spec.js
+++ test/entity-server.spec.js
@@ -3,7 +3,7 @@ const helpers = require('yeoman-test');
 
 describe('jhipster-jooq:entity-server sub-generator', function () {
     this.timeout(30000);
-    describe('any configuration', () => {
+    describe('default config', () => {
         let runResult;
         before(() => {
             return helpers
@@ -14,6 +14,7 @@ describe('jhipster-jooq:entity-server sub-generator', function () {
                     defaults: true,
                     blueprints: 'jooq',
                     withEntities: true,
+                    skipPrompts: true,
                     applicationWithEntities: {
                         config: {
                             baseName: 'jhipster',
@@ -28,12 +29,46 @@ describe('jhipster-jooq:entity-server sub-generator', function () {
                 });
         });
 
-        it('writes repository for the entity', () => {
+        it('should write repository for the entity', () => {
             runResult.assertFile('src/main/java/com/mycompany/myapp/repository/FooJOOQRepository.java');
         });
 
-        it('writes repository implementation for the entity', () => {
+        it('should write repository implementation for the entity', () => {
             runResult.assertFile('src/main/java/com/mycompany/myapp/repository/FooJOOQRepositoryImpl.java');
+        });
+    });
+
+    describe('when jooq is disabled at entity level', () => {
+        let runResult;
+        before(() => {
+            return helpers
+                .create('jhipster:app')
+                .withOptions({
+                    fromCli: true,
+                    skipInstall: true,
+                    defaults: true,
+                    blueprints: 'jooq',
+                    withEntities: true,
+                    applicationWithEntities: {
+                        config: {
+                            baseName: 'jhipster',
+                        },
+                        entities: [{ name: 'Foo', jooq: false }],
+                    },
+                })
+                .withLookups([{ npmPaths: path.join(__dirname, '..', 'node_modules') }, { packagePaths: path.join(__dirname, '..') }])
+                .run()
+                .then(result => {
+                    runResult = result;
+                });
+        });
+
+        it('should not write repository for the entity', () => {
+            runResult.assertNoFile('src/main/java/com/mycompany/myapp/repository/FooJOOQRepository.java');
+        });
+
+        it('should not write repository implementation for the entity', () => {
+            runResult.assertNoFile('src/main/java/com/mycompany/myapp/repository/FooJOOQRepositoryImpl.java');
         });
     });
 });
