a33d1672e67b4f799577cde92afc1258e3c0958d
Marcelo Shima <marceloshima@gmail.com>
2021-09-02 10:26:51 -0300

    Bump jooq and jooq-gradle

--- generators/server/index.js
+++ generators/server/index.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 
-const DEFAULT_JOOQ_VERSION = '3.14.8';
-const DEFAULT_JOOQ_GRADLE_PLUGIN_VERSION = '5.2.1';
+const DEFAULT_JOOQ_VERSION = '3.15.2';
+const DEFAULT_JOOQ_GRADLE_PLUGIN_VERSION = '6.0.1';
 
 const JOOQ_FAMILY_MAPPING = {
   postgresql: 'Postgres',
