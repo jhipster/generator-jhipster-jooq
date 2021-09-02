ef71d71a43ddf2782ababe54316651266e95465f
Marcelo Shima <marceloshima@gmail.com>
2021-09-02 09:15:39 -0300

    Update generato-jhipster and add mocha-expect-snapshot dependency.

---
+++ .mocharc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  recursive: true,
+  reporter: 'spec',
+  slow: 0,
+  timeout: 30000,
+  ui: 'bdd',
+  extension: ['js', 'spec.cjs', 'spec.mjs'],
+  require: 'mocha-expect-snapshot',
+};
