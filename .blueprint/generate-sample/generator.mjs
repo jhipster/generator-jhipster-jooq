f91155136542fab7e36ec625275e2b1c0d809607
mshima <1417183+mshima@users.noreply.github.com>
2024-06-12 15:53:12 +0000

    Update generator-jhipster to 8.5.0

--- .blueprint/generate-sample/generator.mjs
+++ .blueprint/generate-sample/generator.mjs
@@ -1,18 +1,24 @@
 import { readdir } from 'node:fs/promises';
+import { readFileSync } from 'node:fs';
 import BaseGenerator from 'generator-jhipster/generators/base';
-import command from './command.mjs';
 
 export default class extends BaseGenerator {
   sampleName;
+  all;
+
+  constructor(args, opts, features) {
+    super(args, opts, { ...features, jhipsterBootstrap: false });
+  }
 
   get [BaseGenerator.INITIALIZING]() {
     return this.asInitializingTaskGroup({
+      async parseCommand() {
+        await this.parseCurrentJHipsterCommand();
+      },
       async initializeOptions() {
-        this.parseJHipsterArguments(command.arguments);
         if (this.sampleName && !this.sampleName.endsWith('.jdl')) {
           this.sampleName += '.jdl';
         }
-        this.parseJHipsterOptions(command.options);
       },
     });
   }
@@ -20,15 +26,7 @@ export default class extends BaseGenerator {
   get [BaseGenerator.PROMPTING]() {
     return this.asPromptingTaskGroup({
       async askForSample() {
-        if (!this.sampleName) {
-          const answers = await this.prompt({
-            type: 'list',
-            name: 'sampleName',
-            message: 'which sample do you want to generate?',
-            choices: async () => readdir(this.templatePath('samples')),
-          });
-          this.sampleName = answers.sampleName;
-        }
+        await this.promptCurrentJHipsterCommand();
       },
     });
   }
@@ -36,7 +34,11 @@ export default class extends BaseGenerator {
   get [BaseGenerator.WRITING]() {
     return this.asWritingTaskGroup({
       async copySample() {
-        this.copyTemplate(`samples/${this.sampleName}`, this.sampleName, { noGlob: true });
+        if (this.all) {
+          this.copyTemplate('samples/*.jdl', '');
+        } else {
+          this.copyTemplate(`samples/${this.sampleName}`, this.sampleName, { noGlob: true });
+        }
       },
     });
   }
@@ -44,13 +46,17 @@ export default class extends BaseGenerator {
   get [BaseGenerator.END]() {
     return this.asEndTaskGroup({
       async generateSample() {
+        const packageJson = JSON.parse(readFileSync(new URL('../../package.json', import.meta.url)));
+        const projectVersion = `${packageJson.version}-git`;
+
         await this.composeWithJHipster('jdl', {
-          generatorArgs: [this.sampleName],
+          generatorArgs: this.all ? await readdir(this.templatePath('samples')) : [this.sampleName],
           generatorOptions: {
             skipJhipsterDependencies: true,
             insight: false,
             skipChecks: true,
-            skipInstall: true,
+            projectVersion,
+            ...(this.all ? { workspaces: true, monorepository: true } : { skipInstall: true }),
           },
         });
       },
